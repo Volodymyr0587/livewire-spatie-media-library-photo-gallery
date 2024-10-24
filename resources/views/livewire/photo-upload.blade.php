19b3765c28d34e84e386ffe4ad6b70722e479d40
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-10-24 20:43:35 +0300

    Change title on Upload photo page

--- resources/views/livewire/photo-upload.blade.php
+++ resources/views/livewire/photo-upload.blade.php
@@ -1,7 +1,7 @@
 <div class="p-12">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('My Photos') }}
+            {{ __('Upload Photo') }}
         </h2>
     </x-slot>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
