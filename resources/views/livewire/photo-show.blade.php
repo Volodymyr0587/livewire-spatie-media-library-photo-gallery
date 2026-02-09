46ac250889df9496d8407f0a2579c0b3d8c2153c
Volodymyr Honcharov <ladimirvm87@gmail.com>
2026-02-09 17:27:45 +0200

    show photo on full size

--- resources/views/livewire/photo-show.blade.php
+++ resources/views/livewire/photo-show.blade.php
@@ -1,7 +1,30 @@
 <div class="mt-12 bg-white rounded-lg shadow-md overflow-hidden max-w-4xl mx-auto">
-    <div class="flex justify-center ">
-        <img src="{{ $photo->getFirstMediaUrl('photos') }}" alt="{{ $photo->title }}" class="h-auto max-w-full">
+    <div x-data="{ fullscreen: false, imageSrc: '' }">
+        <!-- Your image thumbnail -->
+        <img
+            src="{{  $photo->getFirstMediaUrl('photos') }}"
+            @click="fullscreen = true; imageSrc = $event.target.src"
+            class="cursor-pointer hover:opacity-80 transition"
+            alt="Click to view fullscreen"
+        >
+        <!-- Fullscreen overlay -->
+        <div
+            x-show="fullscreen"
+            x-cloak
+            @click="fullscreen = false"
+            class="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-90"
+        >
+            <img :src="imageSrc" class="max-h-screen max-w-screen p-4" @click.stop>
+            <!-- Close button -->
+            <button
+                @click="fullscreen = false"
+                class="absolute top-4 right-4 text-white text-4xl hover:text-gray-300"
+            >
+                &times;
+            </button>
+        </div>
     </div>
+
     <div class="p-4">
         <h3 class="text-lg font-semibold text-center">{{ $photo->title }}</h3>
         <div class="mt-2 text-center">
